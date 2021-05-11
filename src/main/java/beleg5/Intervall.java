a571f31db00d91ec5364ebfd4ae3a7b6c0581f13
MVVN <Minh.Vu@Student.HTW-berlin.de>
2021-05-11 20:37:21 +0200

    beleg5 - TODO: main

--- src/main/java/beleg5/Intervall.java
+++ src/main/java/beleg5/Intervall.java
@@ -1,4 +1,48 @@
 package beleg5;
 
-public class Intervall {
+public class Intervall<T extends Comparable<? super T>> {
+
+    private T untereGrenze, obereGrenze;
+
+    public Intervall(T untereGrenze, T obereGrenze) {
+        if (untereGrenze != null && obereGrenze != null) {
+            this.untereGrenze = untereGrenze;
+            this.obereGrenze = obereGrenze;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public T getUntereGrenze() {
+        return untereGrenze;
+    }
+
+    public T getObereGrenze() {
+        return obereGrenze;
+    }
+
+    public boolean isLeer() {
+        return untereGrenze.compareTo(obereGrenze) > 0;
+    }
+
+    public boolean enthaelt(T wert) {
+        return wert.compareTo(untereGrenze) >= 0 && wert.compareTo(obereGrenze) <= 0;
+    }
+
+    public <T1 extends T> Intervall<T> schnitt(Intervall<T1> anderes) throws Exception {
+        if (anderes.isLeer() || this.isLeer()) {
+            throw new Exception("Kann keine Schnittmenge mit leerer Menge bilden.");
+        }
+        T untenNeu, obenNeu;
+
+        untenNeu = untereGrenze.compareTo(anderes.getUntereGrenze()) <= 0 ? untereGrenze : anderes.getUntereGrenze();
+        obenNeu = obereGrenze.compareTo(anderes.getObereGrenze()) >= 0 ? obereGrenze : anderes.getObereGrenze();
+
+        return new Intervall<T>(untenNeu, obenNeu);
+    }
+
+    @Override
+    public String toString() {
+        return "[ " + untereGrenze + ", " + obereGrenze + " ]";
+    }
 }
---
+++ src/main/java/beleg5/Main5.java
@@ -0,0 +1,14 @@
+package beleg5;
+
+public class Main5 {
+
+    public static void main(String[] args) {
+
+        Integer int1 = 1;
+        Integer int2 = 3;
+
+        System.out.println(int1.compareTo(null));
+
+    }
+
+}
