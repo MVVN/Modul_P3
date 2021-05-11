1456ae3c4f6d578484b3aaaf5cf81761a020a2cc
MVVN <Minh.Vu@Student.HTW-Berlin.de>
2021-05-11 22:38:04 +0200

    beleg5 finished

--- src/main/java/beleg5/Intervall.java
+++ src/main/java/beleg5/Intervall.java
@@ -1,9 +1,23 @@
 package beleg5;
 
+/**
+ * mathematisches Intervall auf linear angeordneter Menge
+ * @param <T> T muss Comparable implementieren, damit Objekte untereinander vergleichbar sind
+ *           Comparable {@literal <? super T>} Objekte müssen Oberklasse von T sein
+ */
 public class Intervall<T extends Comparable<? super T>> {
 
+    /**
+     * untere und obere Grenze des Intervalls
+     */
     private T untereGrenze, obereGrenze;
 
+    /**
+     * Konstruktor mit oberer und unterer Grenze
+     * @param untereGrenze der niedrigste Wert im Intervall
+     * @param obereGrenze der höchste Wert im Intervall
+     * @throws IllegalArgumentException wenn einer der Parameter null ist
+     */
     public Intervall(T untereGrenze, T obereGrenze) {
         if (untereGrenze != null && obereGrenze != null) {
             this.untereGrenze = untereGrenze;
@@ -13,34 +27,59 @@ public class Intervall<T extends Comparable<? super T>> {
         }
     }
 
+    /**
+     * @return untere Grenze des Intervalls
+     */
     public T getUntereGrenze() {
         return untereGrenze;
     }
 
+    /**
+     * @return obere Grenze des Intervalls
+     */
     public T getObereGrenze() {
         return obereGrenze;
     }
 
+    /**
+     * kontrolliert, ob Grenzen vertauscht sind (null nicht erlaubt, daher nur das kontrollierbar)
+     * @return true, falls vertauscht; ansonsten false
+     */
     public boolean isLeer() {
         return untereGrenze.compareTo(obereGrenze) > 0;
     }
 
+    /**
+     * kontrolliert, ob Wert zwischen unterer und oberer Grenze liegt
+     * @param wert der Wert, nachdem im Intervall geschaut wird
+     * @return true wenn enthalten; false wenn nicht
+     */
     public boolean enthaelt(T wert) {
         return wert.compareTo(untereGrenze) >= 0 && wert.compareTo(obereGrenze) <= 0;
     }
 
+    /**
+     * Bildet Schnittmenge dieser Menge und einer anderen Menge
+     * @param anderes das andere Intervall, mit dem die Schnittmenge gebildet werden soll
+     * @param <T1> Typ der Inhalte im anderen Intervall; muss von T erben, damit Schnittmenge erstellt werden kann
+     * @return Schnittmenge als Intervall
+     * @throws Exception falls eine der beiden Intervalle leer ist
+     */
     public <T1 extends T> Intervall<T> schnitt(Intervall<T1> anderes) throws Exception {
         if (anderes.isLeer() || this.isLeer()) {
             throw new Exception("Kann keine Schnittmenge mit leerer Menge bilden.");
         }
         T untenNeu, obenNeu;
 
-        untenNeu = untereGrenze.compareTo(anderes.getUntereGrenze()) <= 0 ? untereGrenze : anderes.getUntereGrenze();
-        obenNeu = obereGrenze.compareTo(anderes.getObereGrenze()) >= 0 ? obereGrenze : anderes.getObereGrenze();
+        untenNeu = untereGrenze.compareTo(anderes.getUntereGrenze()) <= 0 ? anderes.getUntereGrenze() : untereGrenze;
+        obenNeu = obereGrenze.compareTo(anderes.getObereGrenze()) >= 0 ? anderes.getObereGrenze() : obereGrenze;
 
         return new Intervall<T>(untenNeu, obenNeu);
     }
 
+    /**
+     * @return Intervall im Format "[ a, b ]" als String
+     */
     @Override
     public String toString() {
         return "[ " + untereGrenze + ", " + obereGrenze + " ]";
--- src/main/java/beleg5/Main5.java
+++ src/main/java/beleg5/Main5.java
@@ -1,13 +1,48 @@
 package beleg5;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.sql.Time;
+import java.util.GregorianCalendar;
+
 public class Main5 {
 
     public static void main(String[] args) {
 
-        Integer int1 = 1;
-        Integer int2 = 3;
+        Intervall<String> stringIntervall = new Intervall<>("Apfel", "Mango");
 
-        System.out.println(int1.compareTo(null));
+        System.out.println(stringIntervall);
+        System.out.println("Enthält Bananen? - " + stringIntervall.enthaelt("Bananen"));
+        System.out.println("Ist leer? - " + stringIntervall.isLeer());
+
+        Intervall<String> stringIntervall2 = new Intervall<>("Kaki", "Pomelo");
+        System.out.println(stringIntervall2);
+        try {
+            System.out.println("Schnittmenge: " + stringIntervall.schnitt(stringIntervall2));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Intervall<Integer> integerIntervall = new Intervall<>(5, 0);
+        System.out.println("\n"+integerIntervall);
+        System.out.println("Empty? - " + integerIntervall.isLeer());
+
+        System.out.println();
+
+        Calendar cal1 = new GregorianCalendar(1991, 1, 1);
+        Intervall<Date> dateIntervall = new Intervall<Date>(cal1.getTime(), new Date());
+        Calendar cal2 = new GregorianCalendar(1970, 1, 1);
+        Calendar cal3 = new GregorianCalendar(2005, 1, 1);
+        Intervall<Date> dateIntervall2 = new Intervall<Date>(cal2.getTime(), cal3.getTime());
+
+        Intervall<Time> timeIntervall = new Intervall<Time>(new Time(cal2.getTimeInMillis()), new Time(cal3.getTimeInMillis()));
+
+        try {
+            System.out.println("Schnitt Date-Date: " + dateIntervall.schnitt(dateIntervall2));
+            System.out.println("Schnitt Date-Time: " + dateIntervall.schnitt(timeIntervall));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
